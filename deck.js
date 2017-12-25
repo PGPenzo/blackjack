6d598d12eff52289daccfb4398efc65dab717119
Patrick Penzo <pgpenzo@gmail.com>
2017-12-25 12:49:55 -0800

    Fixed the deck creation
    Deck only has card objects and 52 objects in deck (removed 1).

--- deck.js
+++ deck.js
@@ -1,6 +1,7 @@
 // listing the components of cards
-var ranks = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+var ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 var suits = ['H', 'D', 'C', 'S'];
+var cards = [];
 
 //creating the prototype of cards
 class Card {
@@ -13,33 +14,26 @@ class Card {
 // creating the prototype of decks, based on cards
 class Deck {
   constructor() {
-    this.deck = [];
+    this.cards = [];
   }
 
   makeDeck() {
     for (var r = 0; r < ranks.length; r++) {
       for (var s = 0; s < suits.length; s++) {
-        var newCard = (new Card(this.rank = ranks[r], this.suit = suits[s]));
-        this.deck.push(newCard);
+        cards.push(new Card(ranks[r], suits[s]));
       }
     }
-    return this.deck;
+    return this.cards = cards;
   }
 
   shuffleDeck() {
-    for (var j = 0; j < this.deck.length; j++) {
-      var k = Math.floor(Math.random() * this.deck.length);
-      var temp = this.deck[j];
-      this.deck[j] = this.deck[k];
-      this.deck[k] = temp;
+    for (var j = 0; j < this.cards.length; j++) {
+      var k = Math.floor(Math.random() * this.cards.length);
+      var temp = this.cards[j];
+      this.cards[j] = this.cards[k];
+      this.cards[k] = temp;
     }
   }
-
-  // dealCard(player) {
-  //   var dealtCard = this.deck[0];
-  //   player.hand.push(dealtCard);
-  //   this.deck.splice(0,1);
-  // }
 }
 
 module.exports = Deck;
